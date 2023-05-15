1ea44b73a6f3c4ed039d093997a22670cc308f95
Timothy Moniaga <tim.moniaga@gmail.com>
2023-05-15 13:24:43 +1000

    firebase code

---
+++ Pet Collectors/Pet Collectors/Firebase/Breed.swift
@@ -0,0 +1,16 @@
+//
+//  Breed.swift
+//  Pet Collectors
+//
+//  Created by Timothy Moniaga on 13/5/2023.
+//
+
+import UIKit
+import FirebaseFirestoreSwift
+
+class Breed: NSObject, Codable {
+
+    @DocumentID var id: String?
+    var breed: String?
+    
+}
--- Pet Collectors/Pet Collectors/View-Controllers/InfoViewController.swift
+++ Pet Collectors/Pet Collectors/View-Controllers/InfoViewController.swift
@@ -24,24 +24,30 @@ class InfoViewController: UIViewController {
     func setup() {
         
         let labelText = "Description: \n" + (selectedCard?.details ?? "")
+        print(labelText.debugDescription)
         let attributedText = NSMutableAttributedString(string: labelText)
 
         // Set font and size for the first word
-        let range1 = (labelText as NSString).range(of: "Description:")
-        let font1 = UIFont.boldSystemFont(ofSize: 18)
-        attributedText.addAttribute(.font, value: font1, range: range1)
+//        let range1 = (labelText as NSString).range(of: "Description:")
+//        let font1 = UIFont.boldSystemFont(ofSize: 18)
+//        attributedText.addAttribute(.font, value: font1, range: range1)
         
         title = selectedCard?.breed
         
-        detailsLabel.attributedText = attributedText
+        detailsLabel.text = selectedCard?.details
         //detailsLabel.numberOfLines = 0
+        //detailsLabel.sizeToFit()
+        detailsLabel.contentMode = .top
         //detailsLabel.adjustsFontSizeToFitWidth = true
         detailsLabel.layer.borderColor = UIColor.black.cgColor
         detailsLabel.layer.borderWidth = 0.5
         detailsLabel.font = .systemFont(ofSize: 16)
-
         
         statisticsLabel.text = selectedCard?.statistics
+        detailsLabel.textContainerInset = .zero
+        
+        print(detailsLabel.frame.height)
+
         statisticsLabel.numberOfLines = 0
         statisticsLabel.adjustsFontSizeToFitWidth = true
         
@@ -62,13 +68,15 @@ class InfoViewController: UIViewController {
             image.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             image.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
             //image.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            image.widthAnchor.constraint(equalToConstant: (view.frame.width - 20)),
+            image.widthAnchor.constraint(equalToConstant: (view.frame.width - 30)),
             image.heightAnchor.constraint(equalToConstant: 200),
             
             detailsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             detailsLabel.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 20),
+            //detailsLabel.bottomAnchor.constraint(equalTo: statisticsLabel.topAnchor, constant: 20),
             detailsLabel.widthAnchor.constraint(equalToConstant: (view.frame.width - 20)),
-            //detailsLabel.heightAnchor.constraint(equalToConstant: 300),
+            // detailsLabel.heightAnchor.constraint(equalToConstant: detailsLabel.frame.height),
+            //detailsLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 0),
             
             statisticsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             statisticsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
@@ -77,7 +85,6 @@ class InfoViewController: UIViewController {
             
         ])
         // countdownLabel.isHidden = true
-        
         loadImageFromURL(urlString: selectedCard?.imageURL ?? "")
 
     }
